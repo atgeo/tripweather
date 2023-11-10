6dba6a986c87e03c9eac4008a00443ac1f432c29
Georges Kmeid <georgesk117@gmail.com>
2023-11-10 20:19:15 +0200

    Fix default value of host and type of port in server.js

--- server.js
+++ server.js
@@ -20,9 +20,9 @@ app.use('/auth', authRoutes)
 app.use('/user', userRoutes)
 app.use('/trip', tripRoutes)
 
-const port = process.env.PORT || '8080'
+const port = parseInt(process.env.PORT) || 8080
 
-const host = process.env.HOST || '0.0.0.'
+const host = process.env.HOST || '0.0.0.0'
 
 app.listen(port, host, () => console.log('Server Started'))
 
